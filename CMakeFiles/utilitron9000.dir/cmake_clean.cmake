5c25f709104f0e6987a5bb34ea22150ea4f26396
Erratic Unicorn <david.j.wert@gmail.com>
2018-08-26 17:26:50 -0500

    Initial commit

---
+++ CMakeFiles/utilitron9000.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/utilitron9000.dir/main.cpp.o"
+  "libutilitron9000.pdb"
+  "libutilitron9000.1.0.dylib"
+  "libutilitron9000.dylib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/utilitron9000.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
